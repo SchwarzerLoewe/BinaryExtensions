3b959772bd281b7a48875b186e9f6224d500279a
Chris <cmagerabronn110@web.de>
2015-11-14 08:39:12 +0100

    initial upload

---
+++ System.IO.BinaryExtensions/Extensions.cs
@@ -0,0 +1,104 @@
+﻿using System.Runtime.InteropServices;
+
+namespace System.IO.BinaryExtensions
+{
+    public static class Extensions
+    {
+        public static T ReadStruct<T>(this BinaryReader br)
+        {
+            var buffer = new byte[Marshal.SizeOf(typeof(T))];
+
+            T oReturn;
+
+            try
+            {
+                br.BaseStream.Read(buffer, 0, buffer.Length);
+
+                GCHandle handle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+                oReturn = (T)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(T));
+                handle.Free();
+
+                return oReturn;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public static void Write<T>(this BinaryWriter bw, T value)
+            where T : struct
+        {
+            try
+            {
+                // This function copys the structure data into a byte[]
+                var buffer = new byte[Marshal.SizeOf(value)]; //Set the buffer ot the correct size
+
+                GCHandle h = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+                //Allocate the buffer to memory and pin it so that GC cannot use the space (Disable GC)
+                Marshal.StructureToPtr(value, h.AddrOfPinnedObject(), false);
+                // copy the struct into int byte[] mem alloc 
+                h.Free(); //Allow GC to do its job
+
+                bw.Write(buffer); // return the byte[] . After all thats why we are here right.
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static void Write(this BinaryWriter bw, MemoryStream value)
+        {
+            bw.Write(value.Length);
+            bw.Write(value.ToArray());
+        }
+        public static MemoryStream ReadMemoryStream(this BinaryReader br)
+        {
+            var c = br.ReadInt32();
+
+            return new MemoryStream(br.ReadBytes(c));
+        }
+
+        public static void Write(this BinaryWriter bw, Guid value)
+        {
+            bw.Write(value.ToByteArray().Length);
+            bw.Write(value.ToByteArray());
+        }
+        public static Guid ReadGuid(this BinaryReader br)
+        {
+            var c = br.ReadInt32();
+            return new Guid(br.ReadBytes(c));
+        }
+
+        public static void Write(this BinaryWriter bw, TimeSpan value)
+        {
+            bw.Write(value.Milliseconds);
+        }
+        public static TimeSpan ReadTimespan(this BinaryReader br)
+        {
+            return TimeSpan.FromMilliseconds(br.ReadInt32());
+        }
+
+        public static void Write(this BinaryWriter bw, DateTime value)
+        {
+            bw.Write(value.ToBinary());
+        }
+        public static DateTime ReadDateTime(this BinaryReader br)
+        {
+            return DateTime.FromBinary(br.ReadInt64());
+        }
+
+        public static void Write(this BinaryWriter bw, Version value)
+        {
+            bw.Write(value.Major);
+            bw.Write(value.Minor);
+            bw.Write(value.Revision);
+            bw.Write(value.Build);
+        }
+        public static Version Read(this BinaryReader br)
+        {
+            return new Version(br.ReadInt16(), br.ReadInt16(), br.ReadInt16(), br.ReadInt16());
+        }
+
+    }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.IO.BinaryExtensions;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var fs = new FileStream("l.bin", FileMode.OpenOrCreate))
+            {
+                var s = new S();
+                s.hello = 5;
+                s.world = "hgtr";
+                s.g = new ST() { blub = 155693 };
+
+                var bw = new BinaryWriter(fs);
+                bw.Write(s);
+            }
+
+            using (var fs = new FileStream("l.bin", FileMode.OpenOrCreate))
+            {
+                var bw = new BinaryReader(fs);
+                var s = bw.ReadStruct<S>();
+            }
+        }
+        
+        public struct S
+        {
+            public int hello;
+            public string world;
+            public ST g;
+        }
+
+        public class ST
+        {
+            public int blub;
+        }
+    }
+}
